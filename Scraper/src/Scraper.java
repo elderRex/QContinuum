d8c097a070a686f2a5c7bcc08dd2031553de7ca4
Fyodor Minakov <fom941@g.harvard.edu>
2017-04-05 19:08:11 -0400

    modified scraper now working

--- Scraper/src/Scraper.java
+++ Scraper/src/Scraper.java
@@ -22,12 +22,12 @@ public class Scraper {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 
-		//extractReviewsHTML();
-		downloadHTMLTrainNeg();
+		extractPosReviewsHTML();
+		//downloadHTMLTrainPos();
 
 	}
 	
-	public static void extractReviewsHTML() throws IOException
+	public static void extractPosReviewsHTML() throws IOException
 	{
 		String uhome = System.getProperty("user.home");
 		File file = new File(uhome + "/Dropbox/ASE/Extracted/pos_extracted.txt");
@@ -35,17 +35,16 @@ public class Scraper {
 		FileWriter fw = new FileWriter(file);
 		BufferedWriter bw = new BufferedWriter(fw);
 
-
 		int count = 0;
-
 		
-		for (int i = 0; i >= 0; i--)
+		for (int i = 12087; i >= 0; i--)
 		{
-			Document doc = Jsoup.parse(new File(uhome+"/Dropbox/ASE/HTML/pos_movie_"+count+".html"), "UTF-8");
+			Document doc = Jsoup.parse(new File(uhome+"/Dropbox/ASE/HTML/pos_movie_"+i+".html"), "UTF-8");
 			Elements metaTags = doc.getElementsByTag("meta");
 			
 			// Get title and save to file
 			String title = doc.select("#tn15title>h1>a").text();
+					
 			bw.write("-ttl-:" + title);
 			bw.write("\n\n");
 			
@@ -54,14 +53,37 @@ public class Scraper {
 			while (true)
 			{
 				String review = doc.select("#tn15content > p:nth-child(" + iterator + ")").text();
-				bw.write("\n\n");
-				bw.write("-rev-:"+review);
-				if (review.length() == 0 || review.contains("Add another review"))
+				
+				// If blank review, increment iterator by 1 because we haven't found it.
+				if (review.length() == 0)
+				{
+					iterator++;
+					continue;
+				}
+				
+				// Last node on page always has "Add another review" so skip
+				if (review.contains("Add another review"))
 					break;
+				
+				bw.write("\n\n");
+				bw.write("-rev-:"+ review);
+				// Try to find a review score, and if not, report average of 7.5 for positive, and 2.5 for negative
+				String score = "";
+				try
+				{
+					score = doc.select("#tn15content > div:nth-child(" + (iterator-1) + ") >img").attr("alt").split("/")[0];
+					if (score.length() == 0)
+						score = "7.5";
+				}
+				catch (Exception e)
+				{
+					
+				}
+				
+				bw.write("\n" + score);
 				iterator += 4;
 			}
 			bw.write("\n\n\n");
-
 			
 		}
 		bw.close();
@@ -70,37 +92,30 @@ public class Scraper {
 	}
 	
 	// Downloads HTML from IMDB Training Set
-	public static void downloadHTMLTrainNeg() throws IOException, InterruptedException
+	public static void downloadHTMLTrainPos() throws IOException, InterruptedException
 	{
 		String uhome = System.getProperty("user.home");
 		
 		// Read file containing urls
-		File file = new File(uhome + "/Dropbox/ASE/aclImdb/train/urls_neg.txt");
+		File file = new File(uhome + "/Dropbox/ASE/aclImdb/train/urls_pos.txt");
 		FileReader fileReader = new FileReader(file);
 		BufferedReader bufferedReader = new BufferedReader(fileReader);
 		
 		StringBuffer stringBuffer = new StringBuffer();
 		String url;
-		int count = 3668;
+		int count = 0;
 		
 		while ((url = bufferedReader.readLine()) != null) {
-			try
-			{
-				Response response = Jsoup.connect(url).execute();
-		        Document doc = response.parse();
-	
-		        File f = new File(uhome+"/Dropbox/ASE/HTMLNEG/negative_movie_"+count+".html");
-		        count++;
-		        f.createNewFile();
-		        Random rand = new Random(); 
-		        int value = rand.nextInt(8); 
-		        TimeUnit.SECONDS.sleep(value);
-		        FileUtils.writeStringToFile(f, doc.outerHtml(), "UTF-8");
-			}
-			catch (Exception e)
-			{
-				
-			}
+			Response response = Jsoup.connect(url).execute();
+	        Document doc = response.parse();
+
+	        File f = new File(uhome+"/Dropbox/ASE/HTML/pos_movie_"+count+".html");
+	        count++;
+	        f.createNewFile();
+	        Random rand = new Random(); 
+	        int value = rand.nextInt(8); 
+	        TimeUnit.SECONDS.sleep(value);
+	        FileUtils.writeStringToFile(f, doc.outerHtml(), "UTF-8");
 		}
 	}
 	
