c065086ef07a97e808a0c2c0f804fc66a80571e4
Yoki <camillemagic@gmail.com>
2017-04-06 18:22:44 -0400

    Create loginTest.java

---
+++ QCApp/src/test/resources/loginTest.java
@@ -0,0 +1,22 @@
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class loginTest {
+	
+	@Test
+	public void testSuccessfulLogin() {
+    Login account = new Login();
+		
+    
+		assertEquals();
+	}
+	
+	@Test
+	public void testFailedLogin() {
+		
+	}
+
+}
