0214a869349aee0bdb8f18414bf9f4ab32e3be3c
Yoki <camillemagic@gmail.com>
2017-04-07 01:00:25 -0400

    Update userSetupQuestionTest.java

--- QCApp/src/test/resources/userSetupQuestionTest.java
+++ QCApp/src/test/resources/userSetupQuestionTest.java
@@ -22,7 +22,7 @@ public class userSetupQuestionTest {
               HttpServletRequest request = mock(HttpServletRequest.class);
               //Authentication authentication = mock(Authentication.class);
               //principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-              principal.getUserPrincipal();
+              principal = request.getUserPrincipal();
               HomeController hc = new HomeController();
               System.out.println(principal);
               assertNotNull(hc.getUserSetupQuestions(principal, request));
