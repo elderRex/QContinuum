7df639db80c762905567ce43ef4cd5eb2ddc3011
Yoki <camillemagic@gmail.com>
2017-04-06 20:56:19 -0400

    Update createUserTest.java

--- QCApp/src/test/resources/createUserTest.java
+++ QCApp/src/test/resources/createUserTest.java
@@ -18,7 +18,7 @@ public class createUserTest {
 	HttpServletRequest request = mock(HttpServletRequest.class); 
 	String uinfo = "{“firstname”:”test”, “lastname”: “user”, “email”: “test@columbia.edu”, “password1”: “123”, “password2”: “123”}";
 	HomeController hc = new HomeController();
-	assertEquals("200", hc.createUser(uinfo, request));
+	assertEquals(new ResponseEntity<String>(HttpStatus.BAD_REQUEST), hc.createUser(uinfo, request));
 	}
 	
 	/*
