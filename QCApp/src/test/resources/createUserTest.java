b3106d64c2808cd8315d4a684acf808e77b756cc
Yoki <camillemagic@gmail.com>
2017-04-06 21:02:10 -0400

    Update createUserTest.java

--- QCApp/src/test/resources/createUserTest.java
+++ QCApp/src/test/resources/createUserTest.java
@@ -7,7 +7,9 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import java.io.*;
 import javax.servlet.http.*;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import org.junit.Test;
 
