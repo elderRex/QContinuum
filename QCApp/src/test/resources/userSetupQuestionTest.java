ccb1257b9ad732e45571fa498a9fcef82129b363
Yoki <camillemagic@gmail.com>
2017-04-07 02:18:26 -0400

    Update userSetupQuestionTest.java

--- QCApp/src/test/resources/userSetupQuestionTest.java
+++ QCApp/src/test/resources/userSetupQuestionTest.java
@@ -10,7 +10,7 @@ import java.io.*;
 import javax.servlet.http.*;
 import java.security.Principal;
 
-import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
