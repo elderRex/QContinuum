927e95175461bba1c3c13ab18dfe69cbfce1ab78
Yoki <camillemagic@gmail.com>
2017-04-07 00:30:36 -0400

    Update userSetupQuestionTest.java

--- QCApp/src/test/resources/userSetupQuestionTest.java
+++ QCApp/src/test/resources/userSetupQuestionTest.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.*;
+import org.springframework.security.*;
 
 import org.junit.Test;
 
