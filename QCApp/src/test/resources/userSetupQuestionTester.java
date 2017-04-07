b6ecdc038ae063c653f637e3f1102a62a8cbbe99
Yoki <camillemagic@gmail.com>
2017-04-06 23:35:26 -0400

    Create userSetupQuestionTester.java

---
+++ QCApp/src/test/resources/userSetupQuestionTester.java
@@ -0,0 +1,20 @@
+package QCTeamG.QCApp.controller;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import java.io.*;
+import javax.servlet.http.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+import org.junit.Test;
+
+public class userSetupQuestionTester {
+       @Test
+       public void testSuccessfulQuestion() {
+       
+       }
+}
