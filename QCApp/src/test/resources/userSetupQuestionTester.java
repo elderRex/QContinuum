bb5b59c990016ef00e8f645cc6a62151f2debf94
Yoki <camillemagic@gmail.com>
2017-04-06 23:37:13 -0400

    Update userSetupQuestionTester.java

--- QCApp/src/test/resources/userSetupQuestionTester.java
+++ QCApp/src/test/resources/userSetupQuestionTester.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import java.io.*;
 import javax.servlet.http.*;
+import java.security.Principal;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
@@ -13,8 +14,11 @@ import org.springframework.http.HttpStatus;
 import org.junit.Test;
 
 public class userSetupQuestionTester {
+       private Principal principal;
+       
        @Test
        public void testSuccessfulQuestion() {
+              HomeController hc = new HomeController();
        
        }
 }
