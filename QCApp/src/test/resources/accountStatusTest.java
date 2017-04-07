d8cf1a2528a0cd61a4d0b7bad08eb24ca8e883b6
Yoki <camillemagic@gmail.com>
2017-04-07 15:06:18 -0400

    Update accountStatusTest.java

--- QCApp/src/test/resources/accountStatusTest.java
+++ QCApp/src/test/resources/accountStatusTest.java
@@ -6,24 +6,32 @@ import java.io.*;
 import javax.servlet.http.*;
 import java.security.Principal;
 
+import QCTeamG.QCApp.entities.UsersEntity;
+import QCTeamG.QCApp.dao.UsersDAO;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
 import org.junit.Test;
 
 public class accountStatusTest {
+
   
   @Test
 	public void testAccountFailedLogin() {
+	      
+              	UsersEntity ue = new UsersEntity();
+              
+		
 		HomeController hc = new HomeController();
 		//Principal principal = null;
 		Integer uid = null;
-		String actualstatus = hc.user(uid);
+		String actualstatus = hc.user(uid,ue);
 		String expectedstatus = "home";
 		assertEquals(expectedstatus,actualstatus);
   
   }
-	
+	/*
   @Test
 	public void testAccountSuccessfulSetUp() {
 		HomeController hc = new HomeController();
@@ -34,6 +42,7 @@ public class accountStatusTest {
 		
   
   }
+  */
 	
 /*
   @Test
