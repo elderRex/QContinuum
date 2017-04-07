8cb1ffbb446931b06e20c652fcedd5c86bc9a1b8
XiaZhengyang <xiazhengyang@hku.hk>
2017-04-07 15:38:52 -0400

    Update logoutHandlerTest.java

--- QCApp/src/test/resources/logoutHandlerTest.java
+++ QCApp/src/test/resources/logoutHandlerTest.java
@@ -14,6 +14,7 @@ import QCTeamG.QCApp.dao.UsersDAO;
 
 import org.junit.Test;
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 
 public class logoutHandlerTest extends SimpleUrlLogoutSuccessHandler {
