fef10a7a9c424fb645e50711df3f91390b87f9c1
Yoki <camillemagic@gmail.com>
2017-04-07 14:34:22 -0400

    Create accountStatusTest.java

---
+++ QCApp/src/test/resources/accountStatusTest.java
@@ -0,0 +1,19 @@
+package QCTeamG.QCApp.controller;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import java.io.*;
+import javax.servlet.http.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+import org.junit.Test;
+
+public class accountStatusTest {
+  
+  @Test
+	public void testAccountFailedLogin() {
+  
+  }
+}
